9eab81a37cdaa5a84acdd32156f0ca1b5df664c4
sericaer <sericaer@outlook.com>
2022-02-16 13:56:24 +0800

    Add Plugins/NewUIWidget

---
+++ Assets/Scripts/MainScene_Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScene_Player : MonoBehaviour
+{
+
+    public Button button;
+
+    public Text name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
